d9f3ce4a98ca4206d9779ad09d489388e5b71654
itchyboikemmm <kimandreilugatoc10@gmail.com>
2025-09-19 23:31:42 +0800

    final design

--- store/views.py
+++ store/views.py
@@ -6,4 +6,6 @@ def landing_page(request):
     books = Book.objects.all() # gather all books galing sa database
     best_sellers = Book.objects.filter(is_best_seller=True)[:8]
     categories = Category.objects.annotate(book_count=Count('book'))
-    return render(request, 'store/landing_page.html', {'books': books, 'best_sellers': best_sellers, 'categories': categories,}) # sends it sa template
+    return render(request, 'store/landing_page.html', {'books': books,
+                                                       'best_sellers': best_sellers,
+                                                       'categories': categories,}) # sends it sa template
